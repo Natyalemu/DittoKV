761585f7a03602382904159bc7d8af4c9021745a
hareer here <clashclash77999@gamil.com>
2025-08-20 11:53:47 +0300

    JSON model

--- src/peer.rs
+++ src/peer.rs
@@ -4,15 +4,21 @@ use std::{error, net::ToSocketAddrs};
 // communicate to the rest of the peers.
 use crate::id::{self, Id};
 use std::error::Error;
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 
 struct Peer {
     id: Id,
-    addr: TcpStream,
+    stream: TcpStream,
 }
 
 impl Peer {
-    fn new(id: Id, addr: TcpStream) -> Self {
-        Self { id, addr }
+    fn new(id: Id, stream: TcpStream) -> Self {
+        Self { id, stream }
+    }
+    async fn write<B: AsRef<[u8]>>(&mut self, data: B) -> Result<(), Box<dyn Error>> {
+        self.stream.write_all(data.as_ref()).await?;
+
+        Ok(())
     }
 }
---
+++ src/rpc.rs
@@ -0,0 +1,46 @@
+use crate::{id::Id, log::log::LogEntry};
+use serde::{Deserialize, Serialize};
+
+//This should be updated so that the AppendEntryRequest transports number of LogEntry for efficient
+//communication between leader and followers
+#[derive(Debug, Serialize, Deserialize)]
+struct AppendEntryRequest {
+    pub term: u64,
+    pub leader_id: Id,
+    pub prev_log_index: u64,
+    pub prev_log_term: u64,
+    pub entry: LogEntry,
+    pub leader_commit: u64,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct AppendEntryResponse {
+    pub term: u64,
+    pub success: bool,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct RequestVoteRequest {
+    pub term: u64,
+    pub candidate_id: Id,
+    pub last_login_index: u64,
+    pub last_log_term: u64,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct RequestVoteResponse {
+    pub term: u64,
+    pub vote_granted: bool,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InstallSnapshotRequest {
+    pub term: u64,
+    pub leader_id: u64,
+    pub last_included_index: u64,
+    pub last_included_term: u64,
+    pub offset: u64,
+    pub data: Vec<u8>,
+    pub done: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InstallSnapshotResponse {
+    pub term: u64,
+}
